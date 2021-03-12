554e4d053a2fc58ee1f7447ebcc377682eb2c074
viditpujara-sherbimet <77112396+viditpujara-sherbimet@users.noreply.github.com>
2021-03-12 17:22:55 +0530

    void

--- app/src/main/java/com/project/sherbimet/ApiHelper/WebURL.java
+++ app/src/main/java/com/project/sherbimet/ApiHelper/WebURL.java
@@ -2,8 +2,8 @@ package com.project.sherbimet.ApiHelper;
 
 public class WebURL {
 
-    private static final String KEY_MAIN_URL="https://192.168.43.126/sherbimet/";
-    public static final String KEY_IMAGE_URL="https://192.168.43.126/sherbimet/image/";
+    private static final String KEY_MAIN_URL="http://192.168.43.126/sherbimet/";
+    public static final String KEY_IMAGE_URL="http://192.168.43.126/sherbimet/images/";
     public static final String SERVICE_URL =KEY_MAIN_URL+"service_api.php";
 
 }
